05fd3a6883ebbba79cd8dcc6aaeb945f7fe67453
Chattarin Praiwattananuphan <chattarinpraiwattananuphan@Chattarins-MacBook-Pro.local>
2020-05-09 00:35:52 +0700

    CreateNavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navbar,NavLink,Nav, NavItem,Button,Dropdown,DropdownButton} from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Tabs">
+      <ul class="nav nav-pills ">
+        <li class="nav-item ">
+          <a class="nav-link " href="#">Plan B</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">week3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#">week4</a>
+        </li>
+        <Dropdown className="Dropdown"  >
+          <Dropdown.Toggle  id="dropdown-basic" >
+            Mix Chattarin
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </ul>
+      
     </div>
+      
+    
   );
 }
 
